001b318b58889c735ac3faf5c755c4c66cd260a9
EL KOUAZE Siham <elkouaze.siham@etu.uae.ac.ma>
2024-04-09 15:52:59 +0000

    add user entity

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,7 @@
-import { startStimulusApp } from '@symfony/stimulus-bundle';
+import { Application } from 'stimulus'
 
 const app = startStimulusApp();
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
+
+export default application;
